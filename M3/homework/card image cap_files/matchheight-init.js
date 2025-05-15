d2d355f48861a0ffbf4076117abe92fc4ab89f63
priyanshu10022012 <priyanshu10022012@gmail.com>
2025-05-15 18:27:23 +0530

    initial commit

--- M3/homework/card image cap_files/matchheight-init.js
+++
@@ -1,5 +0,0 @@
-jQuery(function( $ ){
-
-	$('.market-archives .content .entry, .front-page .blog .entry, .category-index .featured-content .entry, .front-page-1 .featured-content .entry-header').matchHeight();
-
-});
