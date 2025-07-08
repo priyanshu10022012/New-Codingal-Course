56aad050051118e3b9108144d35b74d2656d8e11
Your Name <priyanshu10022012@gmail.com>
2025-07-08 21:45:28 +0530

    prev commit

--- M4/classwork/script.js
+++
@@ -1,4 +0,0 @@
-function myfunction() {
-    var a = 4;
-    document.getElementById("demo").innerHTML = a*a;
-}
--- M4/homework/script.js
+++
@@ -1,6 +0,0 @@
-function myfunction() {
-   document.getElementById("result").innerHTML =
-    document.getElementById("demo").firstChild.nodeValue;
-    document.getElementById("result1").innerHTML
-    document.getElementById("demo").childNodes[0].nodeValue;
-}
