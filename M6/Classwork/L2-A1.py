3355368f692beff713618137aef2c13f6d0f3d1c
priyanshu10022012 <priyanshu10022012@gmail.com>
2025-07-23 20:07:35 +0530

    23july

---
+++ M6/Classwork/L2-A1.py
@@ -0,0 +1,11 @@
+age = 15
+is_student = True
+weight = 38.5
+print("Nmae:",name)
+print("Data Type of Nmae is:",type(name))
+print("Age:",age)
+print("is_student:",is_student)
+print("data type of is_student is:",)
+print(":",)
+print(":",)
+print(":",)
