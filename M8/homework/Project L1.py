6d8afd73d73b3b6cf88aefb5e8cd3ec79d52ef0f
Your Name <priyanshu10022012@gmail.com>
2025-08-11 19:00:59 +0530

    Ok

---
+++ M8/homework/Project L1.py
@@ -0,0 +1,36 @@
+my_dict = {}
+my_dict["key"] = "value"
+my_dict[1] = "one"
+my_dict[2] = "two"
+my_dict[3] = "three"
+my_dict[4] = "four"
+my_dict[5] = "five" 
+my_dict[6] = "six"
+my_dict[7] = "seven"
+my_dict[8] = "eight"
+my_dict[9] = "nine" 
+my_dict[10] = "ten" 
+my_dict[11] = "eleven"
+my_dict[12] = "twelve" 
+my_dict[13] = "thirteen"
+my_dict[14] = "fourteen"
+my_dict[15] = "fifteen"
+print(my_dict.values())
+print(my_dict.keys())
+print(my_dict.items())
+print(my_dict.get(1))
+print(my_dict.get(2))
+print(my_dict.get(3))
+print(my_dict.get(4))
+print(my_dict.get(5))
+print(my_dict.get(6))   
+print(my_dict.get(7))
+print(my_dict.get(8))
+print(my_dict.get(9))
+print(my_dict.get(10))
+print(my_dict.get(11))
+print(my_dict.get(12))
+print(my_dict.get(13))
+print(my_dict.get(14))
+print(my_dict.get(15))      
+print(my_dict.get(16, "Key not found"))  # Example of a key that does not exist
