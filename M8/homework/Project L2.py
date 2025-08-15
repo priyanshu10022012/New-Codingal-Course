213894a28f2ccccbb193bd8929d3479d3dc091c2
Your Name <priyanshu10022012@gmail.com>
2025-08-15 20:30:01 +0530

    ok

---
+++ M8/homework/Project L2.py
@@ -0,0 +1,10 @@
+# Tuple to convert
+my_tuple = (1, 2, 3, 4, 5)
+
+# Convert tuple to list
+my_list = list(my_tuple)
+
+# Print the result
+print("Original Tuple:", my_tuple)
+print("Converted List:", my_list)
+        
