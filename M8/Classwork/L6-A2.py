b4860d2daa8ceab9a8f123a6d114e4540cb9cd21
Your Name <priyanshu10022012@gmail.com>
2025-08-27 17:56:55 +0530

    ok

---
+++ M8/Classwork/L6-A2.py
@@ -0,0 +1,16 @@
+class Computer:
+    def __init__(self):
+        self._maxprice = 900
+    def sell(self):
+        print("Selling Price: {}".format(self._maxprice))
+    def setMaxPrice(self, price):
+        self._maxprice = price
+
+c = Computer()
+c.sell()
+
+c._maxprice = 1000
+c.sell()
+
+c.setMaxPrice(1000)
+c.sell()
