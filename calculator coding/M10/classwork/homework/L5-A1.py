bcbbfbfc8a5e5440c8f37a0718e23d38b3145dab
Your Name <priyanshu10022012@gmail.com>
2025-10-06 17:50:27 +0530

    ok

---
+++ calculator coding/M10/classwork/homework/L5-A1.py
@@ -0,0 +1,12 @@
+# check if the number is palidrom or not
+number = int(input("Enter a number: "))
+original_number = number
+reversed_number = 0
+while number > 0:
+    digit = number % 10
+    reversed_number = reversed_number * 10 + digit
+    number //= 10   
+if original_number == reversed_number:
+    print(f"{original_number} is a palindrome.")
+else:
+    print(f"{original_number} is not a palindrome.")
