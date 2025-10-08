3a6178966d54a3f56e390c44eb5067f20942adc1
Your Name <priyanshu10022012@gmail.com>
2025-10-08 17:38:08 +0530

    ok

---
+++ calculator coding/M10/classwork/homework/L6-A1.py
@@ -0,0 +1,13 @@
+# Program to check if given number is prime or not
+from math import sqrt
+number = int(input("Enter a number: "))
+print("\n")
+if number > 1:
+    for i in range(2, int(sqrt(number)) + 1):
+        if number % i == 0:
+            print(number, "is not a prime number.")
+            break
+    else:
+        print(number, "is a prime number.")
+else:
+    print(number, "is not a prime number.")
