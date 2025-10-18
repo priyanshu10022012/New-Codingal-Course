dc1d77f2cb7a7d5f1a80ba5beceaea00a9f695bd
Your Name <priyanshu10022012@gmail.com>
2025-10-18 16:56:22 +0530

    ok

---
+++ calculator coding/M11/Project l1.py
@@ -0,0 +1,8 @@
+print("\nstep-by-step Calculation:")
+print(f"A AND B = {A and B}")
+print(f"B AND C = {B and C}")
+print(f"Q = (A AND B) OR (B AND C) = {(A and B) or (B and C)} = {int(Q)}")
+
+# Final output
+print("\n Final Output:")
+print("Q =", int(Q))
